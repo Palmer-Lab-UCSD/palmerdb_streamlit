7c1d00f6217612dc22624d6e5c43bdc015ee6276
Elaine Keung <95506916+elainekeung@users.noreply.github.com>
2024-03-11 15:53:57 -0700

    initial commit of streamlit app

---
+++ streamlit/pages/Sample Tracking.py
@@ -0,0 +1,236 @@
+"""
+# This page is a dashboard for the sample tracking tables in the database.
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+import hmac
+import components.authenticate as authenticate
+from st_pages import show_pages_from_config, add_indentation, hide_pages
+st.set_page_config(
+    page_title="Palmer Lab Database Samples",
+    page_icon="🐀",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+# Check authentication
+authenticate.set_st_state_vars()
+# Add login/logout buttons
+if st.session_state["authenticated"]:
+    authenticate.button_logout()
+else:
+    authenticate.button_login()
+
+# show pages and hide non-palmer
+add_indentation()
+show_pages_from_config()
+if "palmerlab" not in st.session_state["user_cognito_groups"]:
+    hide_pages(["Database Summary", "Sample Tracking"])
+
+if st.session_state["authenticated"] and "palmerlab" in st.session_state["user_cognito_groups"]:
+
+    st.title('Palmer Lab Database Samples')
+    
+    # for download button
+    @st.cache_data
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    
+    # db connection
+    # creds in secret
+    conn = st.connection("palmerdb", type="sql", autocommit=False)
+    
+    # project list
+    project = conn.query("select project_name from sample_tracking.project_metadata order by project_name")
+    project = project.project_name.tolist()
+    # pool list
+    pool = conn.query("select distinct pool from sample_tracking.sample_barcode_lib where pool != 'None' order by pool")
+    pool = pool.pool.tolist()
+
+    # project selector, rfid filter
+    projects = st.multiselect(label='select project', 
+                       options=project, default=None, 
+                       placeholder="Choose projects", disabled=False, label_visibility="visible", key=1)
+    projects = ','.join([f"'{project}'" for project in projects])
+    rfids = st.text_input('find rfids', key=2)
+    st.write(rfids) # remove later
+    rfids =  ', '.join([f"'{v.strip()}'" for v in rfids.split(',') if v.strip()])
+    st.write(rfids) # remove later
+
+    def build_query(table, options=None, value=None, value2=None, value3=None):
+        ''' 
+        function that builds an sql query based on user input in up to 4 fields
+        being project, rfid, runid (for barcode), pool (for barcode)
+        '''
+        conditions = []
+    
+        if options:
+            conditions.append(f"project_name IN ({options})")
+    
+        if value and value2:
+            conditions.append(f"rfid IN ({value}) AND runid = {value2}")
+        elif value:
+            conditions.append(f"rfid IN ({value})")
+        elif value2:
+            conditions.append(f"runid = {value2}")
+        if value3:
+            conditions.append(f"pool IN ({value3})")
+            
+        query = f"SELECT * FROM sample_tracking.{table}"
+        if conditions:
+            query += " WHERE " + " AND ".join(conditions)
+        return query
+    
+    # tabs
+    tab1, tab2, tab3, tab4, tab5, tab6, tab7 = st.tabs(["Sample Metadata", "DNA Extraction Log", "Sample Barcodes", 'Tissue Received',
+                                                  'Genotyping Logs', 'RNA Received', 'RNA Extraction Log'])
+
+    # sample metadata
+    with tab1:
+        st.header("Sample Metadata")
+        query = build_query('sample_metadata', projects, rfids)
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+        
+        csv = convert_df(df)
+        
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_sample_metadata_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+    
+    # DNA extraction log
+    with tab2:
+        st.header("DNA Extraction Log")
+        query = build_query('extraction_log', projects, rfids)
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+    
+        csv = convert_df(df)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_extraction_log_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+    
+    # sample barcode library
+    with tab3:
+        st.header("Barcode Library")
+    
+        runids = st.text_input('find runid', key=3)
+        runid =  ', '.join([f"'{v.strip()}'" for v in runids.split(',') if v.strip()])
+        pools = st.multiselect(label='select pool', 
+                       options=pool, default=None, 
+                       placeholder="Choose a pool", disabled=False, label_visibility="visible", key=4)
+        pool = ','.join([f"'{v}'" for v in pools])
+    
+        query = build_query('sample_barcode_lib', projects, rfids, runid, pool)
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+        csv = convert_df(df)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_sample_barcode_lib_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+
+    # tissue
+    with tab4:
+        st.header("Tissue Received")
+    
+        query = build_query('tissue', projects, rfids, runid, pool)
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+        csv = convert_df(df)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_tissue_received_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+
+    # genotyping log (combine 10.1, 10.2)
+    with tab5:
+        st.header("Genotyping Logs")
+    
+        rounds = st.multiselect(label='select round', 
+                       options=['10.1', '10.2'], default=None, 
+                       placeholder="Choose a genotyping round", disabled=False, label_visibility="visible", key=5)
+        round = ','.join([f"'{v}'" for v in rounds])
+    
+        query = build_query('genotyping_log_total', projects, rfids)
+        if round:
+            query += f' where pipeline_round in ({round})'
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+        csv = convert_df(df)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_genotyping_log_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+
+    # RNA received
+    with tab6:
+        st.header("RNA Received")
+    
+        query = build_query('rna', projects, rfids)
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+        csv = convert_df(df)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_rna_received_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+
+    # RNA Extraction Log
+    with tab7:
+        st.header("RNA Extraction Log")
+    
+        query = build_query('rna_extraction_log', projects, rfids)
+        st.write(query) # remove later
+        df = conn.query(query)
+        st.dataframe(df)
+        st.write(len(df), ' entries')
+    
+        csv = convert_df(df)
+        st.download_button(
+            label="Download data as CSV",
+            data=csv,
+            file_name=f'n{len(df)}_rna_extraction_log_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+        
+    # force refresh
+    if st.button('Refresh', on_click = st.cache_data.clear()):
+        st.cache_data.clear()
+
+# catch authentication 
+else:
+    if st.session_state["authenticated"]:
+        st.write("You do not have access. Please contact the administrator.")
+    else:
+        st.write("Please login!")
---
+++ streamlit/pages/Summary.py
@@ -0,0 +1,148 @@
+"""
+# This page is an overview for the numbers in the database.
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+import components.authenticate as authenticate
+from st_pages import show_pages_from_config, add_indentation, hide_pages
+st.set_page_config(
+    page_title="Palmer Lab Database Progress Summary",
+    page_icon="🐀",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+
+# Check authentication
+authenticate.set_st_state_vars()
+# Add login/logout buttons
+if st.session_state["authenticated"]:
+    authenticate.button_logout()
+else:
+    authenticate.button_login()
+
+# show pages and hide non-palmer
+add_indentation()
+show_pages_from_config()
+if "palmerlab" not in st.session_state["user_cognito_groups"]:
+    hide_pages(["Database Summary", "Sample Tracking"])
+
+# check authentication again and content
+if st.session_state["authenticated"] and "palmerlab" in st.session_state["user_cognito_groups"]:
+    # db connection
+    # creds in secret
+    conn = st.connection("palmerdb", type="sql", autocommit=False)
+    
+    # project list
+    project = conn.query("select distinct project_name from sample_tracking.project_metadata order by project_name")
+    project = project.project_name.tolist()
+    
+    st.title('Palmer Lab Database Summary')
+    
+    # for download button
+    @st.cache_data
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    
+    options = st.multiselect(label='select project', 
+                   options=project, default=None, 
+                   placeholder="Choose an option", disabled=False, label_visibility="visible")
+    # options = ','.join([f"'{option}'" for option in options])
+
+    # query text
+    sql = '''with tissue as 
+                    (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS tissue
+                    FROM sample_tracking.sample_metadata sm
+                    JOIN sample_tracking.tissue gl ON sm.rfid = gl.rfid
+                    GROUP BY sm.project_name),
+           dna as 
+                   (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS dna_extracted
+                    FROM sample_tracking.sample_metadata sm
+                    JOIN sample_tracking.extraction_log gl ON sm.rfid = gl.rfid
+                    GROUP BY sm.project_name),
+           sequenced as 
+                   (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS sequenced
+                    FROM sample_tracking.sample_metadata sm
+                    JOIN sample_tracking.sample_barcode_lib gl ON sm.rfid = gl.rfid
+                    GROUP BY sm.project_name),
+           genotype as 
+                   (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS genotyped
+                    FROM  sample_tracking.sample_metadata sm
+                    JOIN sample_tracking.genotyping_log_total gl ON sm.rfid = gl.rfid
+                    GROUP BY sm.project_name),
+           redo AS 
+        		(SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS redo
+        		FROM sample_tracking.sample_metadata sm
+        		JOIN sample_tracking.genotyping_log_total gl ON sm.rfid = gl.rfid
+        		WHERE gl.sample_use LIKE 're%'
+        		GROUP BY sm.project_name),
+           ship as (select project_name, count(project_name) as shipped from sample_tracking.sample_metadata group by project_name),
+           rna as 
+                    (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS rna_sent
+                    FROM sample_tracking.sample_metadata sm
+                    JOIN sample_tracking.rna gl ON sm.rfid = gl.rfid
+                    GROUP BY sm.project_name),
+           rna_extract as 
+                   (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS rna_extracted
+                    FROM sample_tracking.sample_metadata sm
+                    JOIN sample_tracking.rna_extraction_log gl ON sm.rfid = gl.rfid
+                    GROUP BY sm.project_name)
+    
+    
+            SELECT 
+                distinct pm.project_name, 
+                coalesce(h.shipped, 0) as shipped,
+                COALESCE(t.tissue, 0) AS tissue, 
+                COALESCE(d.dna_extracted, 0) AS dna_extracted, 
+                COALESCE(s.sequenced, 0) AS sequenced, 
+                COALESCE(g.genotyped, 0) AS genotyped,
+                COALESCE(r.redo, 0) AS redo,
+                COALESCE(rna.rna_sent, 0) AS rna_received,
+                COALESCE(rne.rna_extracted, 0) AS rna_extracted
+            FROM 
+                sample_tracking.project_metadata pm
+            LEFT JOIN tissue t ON pm.project_name = t.project_name
+            LEFT JOIN dna d ON pm.project_name = d.project_name
+            LEFT JOIN sequenced s ON pm.project_name = s.project_name
+            LEFT JOIN genotype g ON pm.project_name = g.project_name
+            LEFT JOIN redo r ON pm.project_name = r.project_name
+            left join ship h on pm.project_name = h.project_name
+            left join rna rna on pm.project_name = rna.project_name
+            left join rna_extract rne on pm.project_name = rne.project_name
+    
+            order by pm.project_name
+    '''
+    # make connection
+    df = conn.query(sql)
+
+    # filter selected projects
+    if len(options) > 0:
+        df = df.loc[df.project_name.isin(options)]
+        
+    # display df, shape
+    st.dataframe(df)
+    
+    st.write(len(df), ' projects')
+
+    # download
+    csv = convert_df(df) 
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name=f'n{len(df)}_{time.strftime("%Y%m%d")}.csv',
+        mime='text/csv',
+    )
+
+    # force refresh
+    if st.button('Refresh', on_click = st.cache_data.clear()):
+        st.cache_data.clear()
+
+# catch authentication 
+else:
+    if st.session_state["authenticated"]:
+        st.write("You do not have access. Please contact the administrator.")
+    else:
+        st.write("Please login!")
