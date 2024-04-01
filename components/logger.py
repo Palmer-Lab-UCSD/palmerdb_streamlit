ee859a849ae5749ed74117116f617490a707353b
Elaine Keung <95506916+elainekeung@users.noreply.github.com>
2024-04-01 13:53:00 -0700

    change authentication, requirements, logger

--- components/logger.py
+++ components/logger.py
@@ -2,20 +2,10 @@ import logging
 from datetime import datetime
 import streamlit as st
 
-log_file = 'session_start_' + datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + ".log"
-
 def setup_logger():
     # Create a logger
     logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-
-    # Create a file handler and set the formatter
-    fh = logging.FileHandler(log_file)
-    formatter = logging.Formatter('%(name)s %(asctime)s %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-
-    # Add the file handler to the logger
-    logger.addHandler(fh)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
     return logger
 
@@ -23,6 +13,5 @@ def log_action(logger, action):
     # Log the action
     logger.info(action)
 
-# Example usage:
 # logger = setup_logger()
 # log_action(logger, "Some action occurred")
--- home.py
+++ home.py
@@ -10,6 +10,8 @@ import yaml
 from yaml.loader import SafeLoader
 from components.logger import *
 import os
+from streamlit_cognito_auth import CognitoAuthenticator
+from dotenv import load_dotenv
 from st_pages import show_pages_from_config, add_indentation, hide_pages
 st.set_page_config(
     page_title="Palmer Lab Database",
@@ -21,33 +23,55 @@ logger = setup_logger()
 filename = os.path.basename(__file__)
 
 log_action(logger, f'{filename}: app started')
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+# with open('config.yaml') as file:
+#     config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+# authenticator = stauth.Authenticate(
+#     config['credentials'],
+#     config['cookie']['name'],
+#     config['cookie']['key'],
+#     config['cookie']['expiry_days'],
+#     config['preauthorized']
+# )
+
+# name, authentication_status, username = authenticator.login('sidebar')
+dotenv_path = os.path.join(os.getcwd(), '.streamlit', 'auth.env')
+load_dotenv(dotenv_path)
+st.write(dotenv_path)
+pool_id = os.environ.get("POOL_ID")
+app_client_id = os.environ.get("APP_CLIENT_ID")
+app_client_secret = os.environ.get("APP_CLIENT_SECRET")
+
+authenticator = CognitoAuthenticator(
+    pool_id=pool_id,
+    app_client_id=app_client_id,
+    app_client_secret=app_client_secret,
+    use_cookies=False
 )
 
-name, authentication_status, username = authenticator.login('sidebar')
+with st.sidebar:
+    is_logged_in = authenticator.login()
 
+    
+username = authenticator.get_username()
 # sidebar pages
 add_indentation()
 show_pages_from_config()
-if authentication_status == None:
+if not is_logged_in:
     hide_pages(["Database Summary", "Sample Tracking"])
-elif authentication_status:
-    log_action(logger, f'{filename}: authentication status: true, user name: {st.session_state["name"]}')
-    authenticator.logout('Logout', 'sidebar')
-    if st.session_state["name"] != 'palmer':
+elif is_logged_in:
+    log_action(logger, f'{filename}: authentication status: true, user name: {username}')
+    def logout():
+        authenticator.logout()
+    with st.sidebar:
+        st.write(f"Welcome, {authenticator.get_username()}")
+        st.button("Logout", "logout_btn", on_click=logout)
+    if username != 'admin':
         hide_pages(["Database Summary", "Sample Tracking"])
-elif authentication_status == False:
-    log_action(logger, f'{filename}: authentication status: false')
-    st.error('Username/password is incorrect')
-    hide_pages(["Database Summary", "Sample Tracking"])
+# elif not is_logged_in:
+#     log_action(logger, f'{filename}: authentication status: false')
+#     st.error('Username/password is incorrect')
+#     hide_pages(["Database Summary", "Sample Tracking"])
 
 
 # title
