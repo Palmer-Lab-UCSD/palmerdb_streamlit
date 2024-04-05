dfe8f860d8d96cf5e3dedc9f414990b5a186a7cc
Elaine Keung <95506916+elainekeung@users.noreply.github.com>
2024-04-05 10:25:27 -0700

    update for authentication and pages"

--- pages/Summary.py
+++ pages/Summary.py
@@ -6,14 +6,13 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import time
-import streamlit_authenticator as stauth
 from components.logger import *
 import os
-import yaml
-from yaml.loader import SafeLoader
+from streamlit_cognito_auth import CognitoAuthenticator
+from dotenv import load_dotenv
 from st_pages import show_pages_from_config, add_indentation, hide_pages
 st.set_page_config(
-    page_title="Palmer Lab Database Progress Summary",
+    page_title="Palmer Lab Database",
     page_icon="🐀",
     layout="wide",
     initial_sidebar_state="auto"
@@ -21,37 +20,44 @@ st.set_page_config(
 logger = setup_logger()
 filename = os.path.basename(__file__)
 
-with open('config.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
+log_action(logger, f'page: {filename}')
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+dotenv_path = os.path.join(os.getcwd(), '.streamlit', 'auth.env')
+load_dotenv(dotenv_path)
+pool_id = os.environ.get("POOL_ID")
+app_client_id = os.environ.get("APP_CLIENT_ID")
+app_client_secret = os.environ.get("APP_CLIENT_SECRET")
+
+authenticator = CognitoAuthenticator(
+    pool_id=pool_id,
+    app_client_id=app_client_id,
+    app_client_secret=app_client_secret,
+    use_cookies=False
 )
 
-name, authentication_status, username = authenticator.login('main')
+with st.sidebar:
+    is_logged_in = authenticator.login()
 
+    
+username = authenticator.get_username()
 # sidebar pages
 add_indentation()
-if authentication_status == None:
-    hide_pages(["Database Summary", "Sample Tracking"])
-    st.write('Please login.')
-elif authentication_status:
-    log_action(logger, f'{filename}: authentication status: true, user name: {st.session_state["name"]}')
-    authenticator.logout('Logout', 'sidebar')
-    if st.session_state["name"] == 'palmer':
-        show_pages_from_config()
-    else:
-        hide_pages(["Database Summary", "Sample Tracking"])
-        st.write("You do not have permission. Please contact the admin if you think this is a mistake.")
-elif authentication_status == False:
-    log_action(logger, f'{filename}: authentication status: false')
-    st.error('Username/password is incorrect')
+show_pages_from_config()
+if not is_logged_in:
+    st.write('Please log in.')
+    hide_pages(["Database Summary", "Sample Tracking", "Data Dictionary"])
+elif is_logged_in:
+    log_action(logger, f'{filename}: authentication status: true, user name: {username}')
+    def logout():
+        authenticator.logout()
+    with st.sidebar:
+        st.write(f"Welcome, {authenticator.get_username()}!")
+        st.button("Logout", "logout_btn", on_click=logout)
+    if username != 'admin':
+        st.write('You do not have permission to access this page :( If you think this is a mistake, please contact us.')
+        hide_pages(["Database Summary", "Sample Tracking", "Data Dictionary"])
 
-if authentication_status and "palmer" in st.session_state["name"]:
+if is_logged_in and username == 'admin':
     # db connection
     # creds in secret
     conn = st.connection("palmerdb", type="sql", autocommit=False)
@@ -76,7 +82,10 @@ if authentication_status and "palmer" in st.session_state["name"]:
     # options = ','.join([f"'{option}'" for option in options])
 
     # query text
-    sql = '''with tissue as 
+    sql = '''
+        rollback;
+        begin transaction;
+           with tissue as 
                     (SELECT sm.project_name, COUNT(DISTINCT gl.rfid) AS tissue
                     FROM sample_tracking.sample_metadata sm
                     JOIN sample_tracking.tissue gl ON sm.rfid = gl.rfid
@@ -165,3 +174,12 @@ if authentication_status and "palmer" in st.session_state["name"]:
     if st.button('Refresh', on_click = st.cache_data.clear()):
         log_action(logger, f'{filename}: refresh button clicked')
         st.cache_data.clear()
+
+with st.sidebar:
+    st.markdown('''
+    [ratgenes.org](https://ratgenes.org)
+    
+    [Palmer Lab website](https://palmerlab.org)
+    
+    [ratgtex.org](https://ratgtex.org)
+    ''')
---
+++ pages/dictionary.py
@@ -0,0 +1,142 @@
+"""
+# This page is a dashboard to view data dictionaries.
+"""
+
+import streamlit as st
+import numpy as np
+import pandas as pd
+from io import StringIO
+import time
+from components.logger import *
+import os
+from streamlit_cognito_auth import CognitoAuthenticator
+from dotenv import load_dotenv
+from st_pages import show_pages_from_config, add_indentation, hide_pages
+st.set_page_config(
+    page_title="Palmer Lab Database",
+    page_icon="🐀",
+    layout="wide",
+    initial_sidebar_state="auto"
+)
+logger = setup_logger()
+filename = os.path.basename(__file__)
+
+log_action(logger, f'page: {filename}')
+
+dotenv_path = os.path.join(os.getcwd(), '.streamlit', 'auth.env')
+load_dotenv(dotenv_path)
+pool_id = os.environ.get("POOL_ID")
+app_client_id = os.environ.get("APP_CLIENT_ID")
+app_client_secret = os.environ.get("APP_CLIENT_SECRET")
+
+authenticator = CognitoAuthenticator(
+    pool_id=pool_id,
+    app_client_id=app_client_id,
+    app_client_secret=app_client_secret,
+    use_cookies=False
+)
+
+with st.sidebar:
+    is_logged_in = authenticator.login()
+
+    
+username = authenticator.get_username()
+# sidebar pages
+add_indentation()
+show_pages_from_config()
+if not is_logged_in:
+    st.write('Please log in.')
+    hide_pages(["Database Summary", "Sample Tracking", "Data Dictionary"])
+elif is_logged_in:
+    log_action(logger, f'{filename}: authentication status: true, user name: {username}')
+    def logout():
+        authenticator.logout()
+    with st.sidebar:
+        st.write(f"Welcome, {authenticator.get_username()}!")
+        st.button("Logout", "logout_btn", on_click=logout)
+    if username != 'admin':
+        st.write('You do not have permission to access this page :( If you think this is a mistake, please contact us.')
+        hide_pages(["Database Summary", "Sample Tracking", "Data Dictionary"])
+
+if is_logged_in and username == 'admin':
+
+    st.title('Data Dictionary')
+    
+    conn = st.connection("palmerdb", type="sql", autocommit=False)
+    log_action(logger, f'{filename}: database connection made')
+    
+    # project list
+    project = conn.query("select project_name from sample_tracking.project_metadata order by project_name")
+    project = project.project_name.tolist()
+    project = [p for p in project if '0' in p]
+    project = sorted(project)
+    log_action(logger, f'{filename}: project list acquired')
+    
+    # for download button
+    @st.cache_data
+    def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv().encode('utf-8')
+    
+    
+    tab1, tab2 = st.tabs(["Data Dictionary by Project", "Search by Trait"])
+    
+    # dd by project
+    with tab1:
+        log_action(logger, f'{filename}: select tab1 data dictionary by project')
+        option = st.selectbox(
+            'Select project: ',
+            (project),
+            index=None)
+        
+        if option is not None:
+            log_action(logger, f'project selected {option}')
+            try:
+                df = pd.read_csv(f'https://palmerlab.s3.sdsc.edu/tsanches_dash_genotypes/gwas_results/{option}/data_dict_{option}.csv')
+                st.dataframe(df)
+                st.write('Total traits: ', len(df))
+                st.download_button(
+                    label="Download data dictionary as CSV",
+                    data=convert_df(df),
+                    file_name=f'n{len(df)}_data_dictionary_{time.strftime("%Y%m%d")}.csv',
+                    mime='text/csv',
+                )
+            except: st.write('Data dictionary not found. Try another project.')
+    
+        
+    
+    
+    with tab2:
+        log_action(logger, f'{filename}: select tab2 search traits')
+        st.write('Search all traits:')
+        data = pd.DataFrame()
+        for p in project:
+            try:
+                dd = pd.read_csv(f'https://palmerlab.s3.sdsc.edu/tsanches_dash_genotypes/gwas_results/{p}/data_dict_{p}.csv')
+                dd['project'] = p
+                data = pd.concat([data, dd])
+            except: continue
+        
+        data = data.iloc[:, :6]
+        trait = st.text_input('Find a trait:', placeholder = 'Enter characters in trait name (e.g. "loco")...')
+        log_action(logger, f'trait searched: {trait}')
+        
+        data = data.loc[data.measure.str.contains(trait)]
+        st.dataframe(data)
+        st.write('Total traits: ', len(data))
+    
+        st.download_button(
+            label="Download data dictionary as CSV",
+            data=convert_df(data),
+            file_name=f'n{len(data)}_traits_dictionary_{time.strftime("%Y%m%d")}.csv',
+            mime='text/csv',
+        )
+        
+with st.sidebar:
+    st.markdown('''
+    [ratgenes.org](https://ratgenes.org)
+    
+    [Palmer Lab website](https://palmerlab.org)
+    
+    [ratgtex.org](https://ratgtex.org)
+    ''')
