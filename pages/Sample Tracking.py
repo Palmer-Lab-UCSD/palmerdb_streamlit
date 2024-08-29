c49b2bfc05ff8bafc7b7fa1c6141e1ae1e56fb1d
Elaine Keung <95506916+elainekeung@users.noreply.github.com>
2024-08-29 15:05:58 -0700

    update sidebar

--- pages/Sample Tracking.py
+++ pages/Sample Tracking.py
@@ -259,4 +259,12 @@ with st.sidebar:
     [Palmer Lab website](https://palmerlab.org)
     
     [ratgtex.org](https://ratgtex.org)
+    
+    Palmer Lab IRS
     ''')
+    with st.container(border=True):
+        st.write('##### :green[Support]')
+        st.markdown("For website support, please contact the Palmer Lab, or Elaine directly at ekeung@health.ucsd.edu.")
+    
+    st.image('./assets/GWAS_1200x150pxBanner-01.png')
+    st.image('https://palmerlab.org/wp-content/uploads/2019/09/palmerlab-logo.png')
