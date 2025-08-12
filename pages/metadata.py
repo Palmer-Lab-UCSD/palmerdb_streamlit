16779c3c34cc83a325bbcc2af140e0b7975d3906
Elaine Keung <95506916+elainekeung@users.noreply.github.com>
2025-08-12 10:01:19 -0700

    typo

--- pages/metadata.py
+++ pages/metadata.py
@@ -100,7 +100,7 @@ if is_logged_in and admin in username:
                 SELECT
                     a.rfid, a.library_name, a.project_name, a.runid as flowcell_id, a.barcode, a.pcr_barcode, a.pool as seq_pool, 'riptide' as seq_method,
                     case when a.rfid LIKE '%CFW%' then 'mouse' when a.project_name like '%su_guo%' then 'zebrafish' when a.project_name like '%friedman%' then 'mouse' when a.project_name like '%huda%' then 'SD' when a.rfid like 'p.cal%' then 'pcal' when else 'rat' end as organism, 
-                    case when a.rfid LIKE '%CFW%' then 'Carworth Farms White' when a.project_name like '%friedman%' then 'Carworth Farms White' when a.project_name like '%su_guo%' then 'Ekkwill zebrafish' else 'Heterogenous stock' when a.project_name like '%huda%' then 'SD' when a.rfid like 'p.cal%' then 'pcal' end as strain, 
+                    case when a.rfid LIKE '%CFW%' then 'Carworth Farms White' when a.project_name like '%friedman%' then 'Carworth Farms White' when a.project_name like '%su_guo%' then 'Ekkwill zebrafish' else 'Heterogeneous stock' when a.project_name like '%huda%' then 'SD' when a.rfid like 'p.cal%' then 'pcal' end as strain, 
                     coalesce({', '.join([f'{string.ascii_lowercase[i]}.sex' for i, project in enumerate(projects, start=1)])}) as sex,
                     coalesce({', '.join([f'{string.ascii_lowercase[i]}.coatcolor' for i, project in enumerate(projects, start=1)])}) as coatcolor,
                     coalesce({', '.join([f'{string.ascii_lowercase[i]}.sires' for i, project in enumerate(projects, start=1)])}) as sires,
